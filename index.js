ee8d93f0db25ef853481f91daafbe579c2765c79
SujalDhiman <116187046+SujalDhiman@users.noreply.github.com>
2023-03-10 14:50:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+let buttonPress=document.querySelectorAll(".btn")
+let display = document.querySelector("#pwd");
+let display2=document.querySelector("#finalDisplay")
+buttonPress.forEach((element,index)=>
+{
+    buttonPress[index].addEventListener("click",()=>
+    {
+        let symbol=buttonPress[index].innerText
+        displayOnInput(symbol)
+    })
+})
+
+function displayOnInput(val1)
+{
+    if (val1 === "+" || val1 === "-" || val1 === "*" || val1 ==="/") 
+    {
+        display.value = display.value+val1;
+    }
+    else if (val1 === ".") 
+    {
+        if (!display.value.includes(".")) 
+        {
+          display.value =display.value+val1;
+        }
+    } 
+    else if (display.value === "0") 
+    {
+        display.value = val1;
+    } 
+    else 
+    {
+        display.value =display.value+val1;
+    }
+}
+
+let finalResult=document.querySelector(".submit")
+finalResult.addEventListener("click",()=>
+{
+    let expression=display.value
+    display2.value=expression
+    let finalAnswer=eval(display.value)
+    if(Number.isInteger(finalAnswer))
+    display.value=finalAnswer
+    else
+    display.value=finalAnswer.toFixed(6)
+})
+
+let reset=document.querySelector("#clear1")
+reset.addEventListener("click",()=>
+{
+   display2.value=""
+   display.value=""
+})
+
+let back=document.querySelector("#back1")
+back.addEventListener("click",remove)
+
+function remove()
+{
+   let letterToDelete=display.value
+   let len=letterToDelete.length
+   letterToDelete=letterToDelete.substring(0,len-1)
+   display.value=letterToDelete
+}
